57fd8586e7355c4f0fa126d5ebb59ed04932d815
reidem <hctrvf@gmail.com>
2024-02-22 10:31:26 +0100

    Rol inicial

---
+++ library/sysload.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import AnsibleModule
+import os
+import psutil
+
+
+def main():
+    module = AnsibleModule(argument_spec={}, supports_check_mode=True)
+    result = dict(changed=False)
+    if module.check_mode:
+        module.exit_json(**result)
+    load_1, load_5, load_15 = os.getloadavg()
+    result['cpu'] = psutil.cpu_percent()
+    result['load_1'] = load_1
+    result['load_5'] = load_5
+    result['load_15'] = load_15
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
